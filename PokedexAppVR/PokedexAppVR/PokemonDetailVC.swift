b4156e7528f68dfadc74a36a214b332d656d12fa
Vladimir Romanov <vladromweb@gmail.com>
2016-07-18 12:55:02 -0700

    added pointers to outlets
    added pointers to outlets

--- PokedexAppVR/PokedexAppVR/PokemonDetailVC.swift
+++ PokedexAppVR/PokedexAppVR/PokemonDetailVC.swift
@@ -12,6 +12,20 @@ class PokemonDetailVC: UIViewController {
     
     var pokemon: Pokemon!
     @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var mainImage: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var defenseLabel: UILabel!
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var pokedexIdLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var baseAttackLabel: UILabel!
+    @IBOutlet weak var currentEvoImage: UIImageView!
+    @IBOutlet weak var nextEvoLabel: UIImageView!
+    @IBOutlet weak var evoLabel: UILabel!
+    @IBAction func backButtonPressed(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
