f33d5e3f220a0ccf478d6f25bf07296374f7aae9
Vladimir Romanov <vladromweb@gmail.com>
2016-10-12 23:02:24 -0700

    Finalized App
    Finalized App

---
+++ PokedexAppVR/PokedexAppVR/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  PokedexAppVR
+//
+//  Created by Volodymyr Romanov on 7/18/16.
+//  Copyright © 2016 Volodymyr Romanov. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
